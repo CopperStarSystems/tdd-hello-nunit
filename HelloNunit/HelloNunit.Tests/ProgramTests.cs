36d312843a985a6118fd445dd7b575ac866686d4
Ed Mays <copperstarsystems@gmail.com>
2016-09-21 15:06:46 -0500

    Stub out initial test for Program.cs

---
+++ HelloNunit/HelloNunit.Tests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿// HelloNunit.HelloNunit.Tests.ProgramTests.cs
+
+using NUnit.Framework;
+
+namespace HelloNunit.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        Program systemUnderTest;
+
+        [Test]
+        public void Constructor_Always_Succeeds()
+        {
+            // Initial stub test
+            Assert.That(systemUnderTest, Is.Not.Null);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            systemUnderTest = new Program();
+        }
+    }
+}
--- HelloNunit/HelloNunit/Program.cs
+++ HelloNunit/HelloNunit/Program.cs
@@ -2,7 +2,7 @@
 
 namespace HelloNunit
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
